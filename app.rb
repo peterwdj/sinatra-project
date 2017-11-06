a1b687622d80749a798219bab8d0326317296df8
Tom Moir <trmoir9@gmail.com>
2017-11-06 15:45:58 +0000

    solved the cat mystery

--- app.rb
+++ app.rb
@@ -13,8 +13,5 @@ get '/new' do
 end
 
 get '/cat' do
-  code = <%= img src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" />
-  erb code
-  # name = 'Cat'
-  # erb :cat
+  erb :cat
 end
