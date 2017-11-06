3d1780ca60d396e2154f79aae53a554058f09042
Peter Johnstone <peter.wdj@gmail.com>
2017-11-06 15:31:28 +0000

    Tried different things to display html in Sinatra - none working

--- app.rb
+++ app.rb
@@ -13,5 +13,8 @@ get '/new' do
 end
 
 get '/cat' do
-    <img src='http://bit.ly/1eze8aE' />
+  code = <%= img src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" />
+  erb code
+  # name = 'Cat'
+  # erb :cat
 end
