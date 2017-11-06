a7f29b6f70687c6d3d44ead8028ee34dc0d16844
Tom Moir <trmoir9@gmail.com>
2017-11-06 15:15:21 +0000

    working on cat image

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "hello!"
 end
+
+get '/secret' do
+  'secret squirrel!'
+end
+
+get '/new' do
+  'stuff'
+end
+
+get '/cat' do
+    <img src='http://bit.ly/1eze8aE' />
+end
